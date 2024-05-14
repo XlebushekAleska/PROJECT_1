2dfd290858cd00b11c81abb6c15a4efbbd3ecba5
XlebushekAleska <zhirnel.ales@gmail.com>
2024-05-14 14:07:42 +0300

    написал функцию получения информации об операциях (transfer, sale, receipt, write_off), написал создание пустой бозы данных, попытался реализовать полиморфизм через генераторы (set, get), а также реализовать приватность данных класса

--- database_class.py
+++ database_class.py
@@ -3,26 +3,36 @@ import sqlite3
 
 class Database:
     def __init__(self, db_name):
-        self.conn = sqlite3.connect(db_name)
-        self.cur = self.conn.cursor()
+        self.__conn = sqlite3.connect(db_name)
+        self.__cur = self.__conn.cursor()
 
-    def get_data(self, table_name, id):
+    # свойство-геттер
+
+    @property
+    def db_data(self):
+        return self.db_data
+
+    @db_data.setter
+    def db_data(self, table_name, data):
+        placeholders = ', '.join(['?' for _ in range(len(data))])
+        query = f"INSERT INTO {table_name} VALUES ({placeholders})"
+        self.__cur.execute(query, data)
+        self.__conn.commit()
+
+    @db_data.getter
+    def db_data(self, table_name, row_id):
         query = f"SELECT * FROM {table_name} WHERE id=?"
-        self.cur.execute(query, (id,))
-        row = self.cur.fetchone()
+        self.__cur.execute(query, (row_id,))
+        row = self.__cur.fetchone()
         if row:
             return row[1]
         else:
             return None
 
-    def set_data(self, table_name, data):
-        placeholders = ', '.join(['?' for _ in range(len(data))])
-        query = f"INSERT INTO {table_name} VALUES ({placeholders})"
-        self.cur.execute(query, data)
-        self.conn.commit()
+
 
     def table_filling(self, table_name):
-        cursor = self.cur.execute(f"SELECT * FROM {table_name}")
+        cursor = self.__cur.execute(f"SELECT * FROM {table_name}")
         data = cursor.fetchall()
         column_names = [description[0] for description in cursor.description]
         return data, column_names
@@ -30,36 +40,46 @@ class Database:
 
 
     def operations(self):
+        # возвращает тип операции, дату операции, id скалада
         transfer_query = f"""
-            SELECT * FROM Transfer 
-                JOIN Sale ON Transfer.from_warehouse_id = 
-
-        """
+                            SELECT transfer_date, from_warehouse_id, to_warehouse_id FROM Transfer 
+                          """
 
         sale_query = f"""
-                    SELECT * FROM Transfer 
-                        JOIN Sale ON Transfer.from_warehouse_id = 
-
-                """
+                        SELECT sale_date, warehouse_id FROM Sale 
+                      """
 
         receipt_query = f"""
-                            SELECT * FROM Transfer 
-                                JOIN Sale ON Transfer.from_warehouse_id = 
-
+                            SELECT receipt_date, warehouse_id FROM Receipt 
                         """
 
-        cursor = self.cur.execute(query)
-        data = cursor.fetchall()
-        # возвращает тип операции, дату операции, id скалада
-        pass
+        write_off_query = f"""
+                            SELECT write_off_date, warehouse_id FROM Write_off 
+                           """
+
+        cursor = self.__cur.execute(transfer_query)
+        transfer_data = ('transfer', cursor.fetchall())
+        cursor = self.__cur.execute(sale_query)
+        sale_data = ('sale', cursor.fetchall())
+        cursor = self.__cur.execute(receipt_query)
+        receipt_data = ('receipt', cursor.fetchall())
+        cursor = self.__cur.execute(write_off_query)
+        write_off_data = ('write_off', cursor.fetchall())
+
+        return transfer_data, sale_data, receipt_data, write_off_data
+
+
 
     def close(self):
-        self.conn.close()
+        self.__conn.close()
 
 
 if __name__ == "__main__":
-    # Создаем объект базы данных
-    db = Database("example.db")
+    db = Database("Database1.db")
+
+
+
+
 
 # from PyQt5 import QtCore, QtGui, QtWidgets
 # import sqlite3 as sl
---
+++ db_maker.py
@@ -0,0 +1,115 @@
+import sqlite3 as sl
+
+con = sl.connect('Database1.db')
+
+with con:
+    con.execute("""
+        CREATE TABLE IF NOT EXISTS Goods (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR(100) NOT NULL,
+            article VARCHAR(500),
+            category VARCHAR(200),
+            charasteristic VARCHAR(500),
+            picture BLOB NOT NULL,
+            price VARCHAR(100) NOT NULL
+        );
+    """)
+
+    con.execute("""
+        CREATE TABLE IF NOT EXISTS Warehouses (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            adress VARCHAR(150) NOT NULL,
+            name VARCHAR(80) NOT NULL,
+            geolocation VARCHAR(150),
+            coordinates VARCHAR(100)
+         );
+    """)
+
+    con.execute("""
+        CREATE TABLE IF NOT EXISTS Clients (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR(200) NOT NULL,
+            contact VARCHAR(200) NOT NULL,
+            comment VARCHAR(300)
+        );
+    """)
+
+    con.execute("""
+            CREATE TABLE IF NOT EXISTS Orders (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                client_id INTEGER NOT NULL,
+                order_date VARCHAR(100) NOT NULL,
+                status VARCHAR(80) NOT NULL,
+                price VARCHAR(100) NOT NULL,
+                comment VARCHAR(500)
+            );
+        """)
+
+    con.execute("""
+                CREATE TABLE IF NOT EXISTS Order_content (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    order_id INTEGER NOT NULL,
+                    goods_id VARCHAR(500) NOT NULL,
+                    count INTEGER NOT NULL,
+                    sum_price VARCHAR(200) NOT NULL,
+                    delivery_date VARCHAR(100) NOT NULL,
+                    expiration_date VARCHAR(100) NOT NULL
+                );
+            """)
+
+    con.execute("""
+                CREATE TABLE IF NOT EXISTS Accounting (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    good_id INTEGER NOT NULL,
+                    warehouse_id INTEGER NOT NULL,
+                    count INTEGER NOT NULL,
+                    delivery_date VARCHAR(100) NOT NULL,
+                    expiration_date VARCHAR(100) NOT NULL
+                );
+            """)
+
+    con.execute("""
+                CREATE TABLE IF NOT EXISTS Write_off (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    good_id INTEGER NOT NULL,
+                    warehouse_id INTEGER NOT NULL,
+                    write_off_date VARCHAR(100) NOT NULL,
+                    count INTEGER NOT NULL,
+                    reason VARCHAR(500) NOT NULL
+                );
+            """)
+
+    con.execute("""
+                CREATE TABLE IF NOT EXISTS Receipt (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    good_id INTEGER NOT NULL,
+                    warehouse_id INTEGER NOT NULL,
+                    receipt_date VARCHAR(100) NOT NULL,
+                    count INTEGER NOT NULL,
+                    comment VARCHAR(500)
+                );
+            """)
+
+    con.execute("""
+                CREATE TABLE IF NOT EXISTS Sale (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    good_id INTEGER NOT NULL,
+                    warehouse_id INTEGER NOT NULL,
+                    client_id INTEGER NOT NULL,
+                    sale_date VARCHAR(100) NOT NULL,
+                    count INTEGER NOT NULL,
+                    price VARCHAR(100) NOT NULL
+                );
+            """)
+
+    con.execute("""
+                    CREATE TABLE IF NOT EXISTS Transfer (
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        good_id INTEGER NOT NULL,
+                        from_warehouse_id INTEGER NOT NULL,
+                        to_warehouse_id INTEGER NOT NULL,
+                        transfer_date VARCHAR(100) NOT NULL,
+                        count INTEGER NOT NULL,
+                        comment VARCHAR(500) NOT NULL
+                    );
+                """)
